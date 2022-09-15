786775126330e7f6d36970ed74d934bbebd7db5a
Davie <107007169+daviethedeveloper@users.noreply.github.com>
2022-09-14 18:08:31 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,56 +1,94 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { CssBaseline, Grid } from '@material-ui/core';
 
-
-import { getPlacesData} from './api';
-
+import { getPlacesData, getWeatherData } from './api/travelAdvisorAPI';
 import Header from './components/Header/Header';
 import List from './components/List/List';
 import Map from './components/Map/Map';
 
 const App = () => {
-    const [places, setPlaces] = useState([]);
+  const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState('');
 
-    const [coordinates, setCoordinates] = useState({});
-    const [bounds, setBounds] = useState(null);
+  const [coords, setCoords] = useState({});
+  const [bounds, setBounds] = useState(null);
 
-    useEffect(() => {
-        navigator.geolocation.getCurrentPosition(({coords: {latitude, longitude}}) => {
-            setCoordinates({ lat: latitude, lng: longitude});
-        })
+  const [weatherData, setWeatherData] = useState([]);
+  const [filteredPlaces, setFilteredPlaces] = useState([]);
+  const [places, setPlaces] = useState([]);
 
-    }, []);
+  const [autocomplete, setAutocomplete] = useState(null);
+  const [childClicked, setChildClicked] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
+      setCoords({ lat: latitude, lng: longitude });
+    });
+  }, []);
 
-    useEffect(() => {
-        console.log(coordinates, bounds);
-        getPlacesData()
+  useEffect(() => {
+    const filtered = places.filter((place) => Number(place.rating) > rating);
+
+    setFilteredPlaces(filtered);
+  }, [rating]);
+
+  useEffect(() => {
+    if (bounds) {
+      setIsLoading(true);
+
+      getWeatherData(coords.lat, coords.lng)
+        .then((data) => setWeatherData(data));
+
+      getPlacesData(type, bounds.sw, bounds.ne)
         .then((data) => {
-            console.log(data);
-            setPlaces(data);
-        })
-    }, [coordinates, bounds]);
+          setPlaces(data.filter((place) => place.name && place.num_reviews > 0));
+          setFilteredPlaces([]);
+          setRating('');
+          setIsLoading(false);
+        });
+    }
+  }, [bounds, type]);
 
-    return (
-        <>
+  const onLoad = (autoC) => setAutocomplete(autoC);
 
-            <CssBaseline />
-            <Header />
-            <Grid container spacing={3} style={{width: '100%'}}>
-                <Grid item xs={12} md={4}>
-                        <List />
-                </Grid>
+  const onPlaceChanged = () => {
+    const lat = autocomplete.getPlace().geometry.location.lat();
+    const lng = autocomplete.getPlace().geometry.location.lng();
 
-                <Grid item xs={12} md={8}>
-                    <Map
-                    setCoordinates={setCoordinates}
-                    setBounds={setBounds}
-                    coordinates={coordinates}
-                    />
-                </Grid>
-            </Grid>
-        </>
-    );
-}
+    setCoords({ lat, lng });
+  };
+
+  return (
+    <>
+      <CssBaseline />
+      <Header onPlaceChanged={onPlaceChanged} onLoad={onLoad} />
+      <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={4}>
+          <List
+            isLoading={isLoading}
+            childClicked={childClicked}
+            places={filteredPlaces.length ? filteredPlaces : places}
+            type={type}
+            setType={setType}
+            rating={rating}
+            setRating={setRating}
+          />
+        </Grid>
+        <Grid item xs={12} md={8} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <Map
+            setChildClicked={setChildClicked}
+            setBounds={setBounds}
+            setCoords={setCoords}
+            coords={coords}
+            places={filteredPlaces.length ? filteredPlaces : places}
+            weatherData={weatherData}
+          />
+        </Grid>
+      </Grid>
+    </>
+  );
+};
 
 export default App;
+
